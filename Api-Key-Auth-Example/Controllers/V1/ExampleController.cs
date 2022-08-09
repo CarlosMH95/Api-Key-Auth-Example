0db5c8613922c2df5e8e9fee6c788916cf07e420
Carlos Manosalvas <cmanosalvas95@gmail.com>
2022-08-09 16:49:33 -0500

    Initial Commit

---
+++ Api-Key-Auth-Example/Controllers/V1/ExampleController.cs
@@ -0,0 +1,16 @@
+﻿using Api_Key_Auth_Example.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Key_Auth_Example.Controllers.V1
+{
+    [ApiKeyAuth]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet(template:"secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I Have no Secrets");
+        }
+    }
+}
---
+++ Api-Key-Auth-Example/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api_Key_Auth_Example.Filters
+{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if(!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            //Get the configuration service
+            var configuration  = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            //get the api key from the configuration
+            var apiKey = configuration.GetValue<string>(key:"ApiKey");
+            //Validate the ApiKey of the request
+            if (!apiKey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+             await next();
+        }
+    }
+}
